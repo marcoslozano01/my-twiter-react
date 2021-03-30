dcf3beaca96051f266d609b95f4b1f7ecc2ef2df
marcos <marcos.lozano01@gmail.com>
2021-03-30 13:20:02 +0200

    React end

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { HashRouter, Route } from 'react-router-dom';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Home from './components/Home';
+import App from './components/App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+//Para posibilitar la renderización en base a un id determinado
+ReactDOM.render(<HashRouter>
+  <div>
+    <Route exact path="/" component={App} />
+    <Route path="/home" component={Home} />
+  </div>
+</HashRouter >, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
