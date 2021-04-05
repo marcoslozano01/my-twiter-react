df6483aa3dc0b19ce5326a9d9fe5833ce3773711
marcos <marcos.lozano01@gmail.com>
2021-04-05 17:00:36 +0200

    Pruebas2

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 import config from '../config.js';
 
 export default axios.create({
-  baseURL: config.baseURL_API
+  //baseURL: config.baseURL_API
+  baseURL:'http://localhost:5000'
 });
